f774a106d0a3624ef47b11c986e6c977e2360d93
hmatsu47 <hmatsu47@gmail.com>
2021-12-04 23:08:18 +0900

    内部ファイル構成変更

--- lib/util_config.dart
+++ lib/util_config.dart
@@ -1,8 +1,58 @@
 import 'dart:io';
 
+import 'package:mapbox_gl/mapbox_gl.dart';
 import 'package:path_provider/path_provider.dart';
 
 import 'class_definition.dart';
+
+// 設定ファイルに保存
+Future<void> configureSave(FullConfigData configData) async {
+  final localPath = (await getApplicationDocumentsDirectory()).path;
+  final File configFile = File('$localPath/${configData.configFileName}');
+  configFile.writeAsStringSync('''style=${configData.style}
+s3AccessKey=${configData.s3AccessKey}
+s3SecretKey=${configData.s3SecretKey}
+s3Bucket=${configData.s3Bucket}
+s3Region=${configData.s3Region}
+''', mode: FileMode.writeOnly);
+}
+
+// 設定ファイルの内容読み込み
+ReadConfigData configureRead(File configFile) {
+  final List<String> config = configFile.readAsLinesSync();
+  final List<String> addStyle = [];
+  String s3AccessKey = '';
+  String s3SecretKey = '';
+  String s3Bucket = '';
+  String s3Region = '';
+  for (String line in config) {
+    final int position = line.indexOf('=');
+    if (position != -1) {
+      final String itemName = line.substring(0, position);
+      final String itemValue = line.substring(position + 1);
+      switch (itemName) {
+        case 'style':
+          addStyle.add(itemValue);
+          // 渋滞状況マップはデフォルトで実装
+          addStyle.add(MapboxStyles.TRAFFIC_DAY);
+          break;
+        case 's3AccessKey':
+          s3AccessKey = itemValue;
+          break;
+        case 's3SecretKey':
+          s3SecretKey = itemValue;
+          break;
+        case 's3Bucket':
+          s3Bucket = itemValue;
+          break;
+        case 's3Region':
+          s3Region = itemValue;
+          break;
+      }
+    }
+  }
+  return ReadConfigData(addStyle, s3AccessKey, s3SecretKey, s3Bucket, s3Region);
+}
 
 // 追加地図設定ファイルに保存
 Future<void> configureExtStyleSave(FullConfigExtStyleData configData) async {
